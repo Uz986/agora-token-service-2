620a10a64ac78b0358c15cb4e7fa8be7cbe5073f
Uz986 <44508403+Uz986@users.noreply.github.com>
2023-01-15 16:03:11 +0000

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,32 @@
 package main
 
 import (
-    "github.com/AgoraIO-Community/agora-token-service/service"
     "net/http"
+    "github.com/AgoraIO-Community/agora-token-service/service"
 )
 
 func CORS(next http.HandlerFunc) http.HandlerFunc {
-  return func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Access-Control-Allow-Origin", "*")
-    w.Header().Add("Access-Control-Allow-Credentials", "true")
-    w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-    w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    return func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Add("Access-Control-Allow-Origin", "*")
+        w.Header().Add("Access-Control-Allow-Credentials", "true")
+        w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
-    if r.Method == "OPTIONS" {
-        http.Error(w, "No Content", http.StatusNoContent)
-        return
+        if r.Method == "OPTIONS" {
+            http.Error(w, "No Content", http.StatusNoContent)
+            return
+        }
+
+        next(w, r)
     }
-
-    next(w, r)
-  }
 }
 
 func main() {
-	s := service.NewService()
+    s := service.NewService()
+
+    // Stop is called on another thread, but waits for an interrupt
+    go s.Stop()
+
     http.HandleFunc("/", CORS(s.Start))
-	go s.Stop()
-	http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":3000", nil)
 }
