abb906c33e8b34b272052d15c6c6953c0bd53360
Uz986 <44508403+Uz986@users.noreply.github.com>
2023-01-15 15:20:07 +0000

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"github.com/AgoraIO-Community/agora-token-service/service"
+	"net/http"
 )
+
+func CORS(next http.HandlerFunc) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Access-Control-Allow-Origin", "*")
+    w.Header().Add("Access-Control-Allow-Credentials", "true")
+    w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+    w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+    if r.Method == "OPTIONS" {
+        http.Error(w, "No Content", http.StatusNoContent)
+        return
+    }
+
+    next(w, r)
+  }
+}
 
 func main() {
 	s := service.NewService()
 	// Stop is called on another thread, but waits for an interrupt
 	go s.Stop()
-	s.Start()
+	http.HandleFunc("/", CORS(s.Start))
+	http.ListenAndServe(":8080", nil)
 }
